52b2f0c7accbf7c1011ceb867e8f87f244659c5e
Ankita 🤍 <ankita06nandy@gmail.com>
2026-09-20 14:31:58 +0530

    Fix CORS for local frontend

--- backend/main.py
+++ backend/main.py
@@ -13,9 +13,9 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173"
+     "https://manraksha.vercel.app"   
     ],
+    allow_origin_regex=r"http://(localhost|127\.0\.0\.1):\d+",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
