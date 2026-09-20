abeba3e3aeb52b754bdbf13397bcd9b8538d635d
Ankita 🤍 <ankita06nandy@gmail.com>
2026-09-21 00:28:59 +0530

    Fix production frontend CORS

--- backend/main.py
+++ backend/main.py
@@ -20,7 +20,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://manraksha.vercel.app"
+        "https://manraksha-app.vercel.app"
     ],
     allow_origin_regex=r"http://(localhost|127\.0\.0\.1):\d+",
     allow_credentials=True,
